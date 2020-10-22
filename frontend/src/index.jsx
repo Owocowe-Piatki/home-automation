a0c82fa45f4cba1f27de03ffdec4fdadeefc80e9
saunter511 <b.barcicki1999@gmail.com>
2020-10-22 16:02:18 +0200

    Moved globalstyles back to index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { ThemeContext, useTheme } from './theme';
-import { GlobalStyle } from './global';
+import { createGlobalStyle } from 'styled-components';
 
 import App from './App';
+
+export const GlobalStyle = createGlobalStyle`
+	html, body, #root {
+		height: 100%;
+		max-width: 100vw;
+		margin: 0;
+		box-sizing: border-box;
+		font-family: Helvetica, Arial, Verdana, Tahoma, sans-serif;
+		transition: 0.2s
+	}
+
+	*, *:before, &:after {
+		box-sizing: inherit;
+	}
+
+    body {
+		background: ${(p) => p.theme.background};
+		color: ${(p) => p.theme.text};
+	}
+`;
 
 const Root = () => {
 	const [theme, toggleTheme, isDark] = useTheme();
