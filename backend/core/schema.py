ca329d107b03fbbdd8fcc4c0249c993a01d30fe5
endero <endero238@gmail.com>
2020-10-23 20:53:32 +0200

    Changed name to birthdays_this_month and deleted user field in schema.py

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -5,7 +5,6 @@ from apps.users.gql.queries import UserQuery
 
 class Query(UserQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
-    user = graphene.Field(UserQuery)
 
 
 schema = graphene.Schema(query=Query)
