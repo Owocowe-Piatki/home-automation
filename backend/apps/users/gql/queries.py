73b728ca81e600885c535a185c61579357956318
endero <endero238@gmail.com>
2020-10-23 20:10:54 +0200

    Added UserTypes and UserQuery for Graphql

---
+++ backend/apps/users/gql/queries.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+import graphene
+
+from ..models import User
+from .types import PublicUserType, UserType
+
+
+class UserQuery(graphene.ObjectType):
+    current_user = graphene.Field(UserType)
+    user_birthday = graphene.List(PublicUserType)
+
+    def resolve_current_user(self, info):
+        return info.context.user
+
+    def resolve_user_birthday(self, info):
+        return User.objects.filter(
+            birth_date__month=datetime.now().month,
+            birth_date__day__gte=datetime.now().day,
+        )
+
+
+schema = graphene.Schema(query=UserQuery)
--- backend/core/schema.py
+++ backend/core/schema.py
@@ -1,8 +1,10 @@
 import graphene
+from apps.users.gql.queries import UserQuery
 
 
-class Query(graphene.ObjectType):
+class Query(UserQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
+    user = graphene.Field(UserQuery)
 
 
 schema = graphene.Schema(query=Query)
