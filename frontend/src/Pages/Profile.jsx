57a90d0d028f536817d3fc3f3c0476b1b3025b95
saunter511 <b.barcicki1999@gmail.com>
2020-10-28 14:39:41 +0100

    Added apollo client

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 // import styled from 'styled-components';
 import PageHeader from 'Theme/Components/PageHeader';
 import PageContainer from 'Theme/Components/PageContainer';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USER = gql`
+	query getUser {
+		currentUser {
+			fullName
+			lastLogin
+		}
+	}
+`;
 
 const Profile = () => {
+	const { loading, error, data } = useQuery(GET_USER);
+	if (loading) return 'Loading...';
+	if (error) return `Error! ${error.message}`;
+
 	return (
 		<PageContainer>
 			<PageHeader>Profile</PageHeader>
+			<p> User: {data.currentUser.fullName} </p>
+			<p> Last login: {data.currentUser.lastLogin} </p>
 		</PageContainer>
 	);
 };
