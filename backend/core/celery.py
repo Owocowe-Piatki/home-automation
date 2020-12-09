0cc45cb817559a237650a853ccf9f3ecbdd23b85
Maciej Rim <maciej.rim@gmail.com>
2020-12-09 16:37:44 +0100

    py3.9 celery fix and cleanup

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -20,6 +20,6 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 # Example task
-# @app.task(bind=True)
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))
+@app.task(bind=True)
+def debug_task(self):
+    return {"id": self.request.id, "task": self.request.task}
