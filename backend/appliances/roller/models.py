f6f3740747bd7a040ad876e89626f54ae3710288
Maciej Rim <maciej.rim@gmail.com>
2020-12-09 18:46:03 +0100

    add fsm to backend and convert roller appliance

--- backend/appliances/roller/models.py
+++ backend/appliances/roller/models.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db import models
+from django_fsm import FSMField, can_proceed, transition
 
 from apps.home.models import Appliance
 
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class Roller(Appliance):
-    open = models.BooleanField("open", default=False)
+    state = FSMField(default="closed", protected=True)
 
     class Meta:
         verbose_name = "Roller"
@@ -17,17 +18,38 @@ class Roller(Appliance):
     def __str__(self):
         return f"<Roller {self.mqtt_topic}>"
 
+    @transition(field=state, source="closed", target="openRequest")
+    def open(self):
+        pass
+
+    @transition(field=state, source="openRequest", target="opened")
+    def opened(self):
+        pass
+
+    @transition(field=state, source="opened", target="closeRequest")
+    def close(self):
+        pass
+
+    @transition(field=state, source="closeRequest", target="closed")
+    def closed(self):
+        pass
+
     def mqtt_message(self, topic, payload):
         """
         Handle the mqtt message passed from the room
         """
-        if payload.lower() not in ("opened", "closed"):
+        payload = payload.lower().strip()
+
+        if payload not in ("opened", "closed", "open", "close"):
             return
 
-        if payload.lower() == "opened":
-            self.open = True
-        elif payload.lower() == "closed":
-            self.open = False
+        method = getattr(self, payload)
 
-        logger.info(f"Roller {self.mqtt_topic} {payload.lower()}")
-        self.save()
+        if can_proceed(method):
+            method()
+            self.save()
+            logger.info(f"Roller {self.mqtt_topic}: {self.state}")
+        else:
+            logger.warning(
+                f"Roller {self.mqtt_topic} can't '{payload}', current state is {self.state}"
+            )
